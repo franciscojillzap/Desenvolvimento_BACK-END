dfa40adac14912a8ec360caf8bbddbb0236423e5
Jill_ZAP <franciscojusepi@gmail.com>
2026-09-01 00:50:55 -0300

    Add files via upload

--- Lista 4/main.py
+++ Lista 4/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime, date
 
 app = Flask(__name__)
 
@@ -8,13 +9,64 @@ def formulario_aluno():
 
 # @app.route("/alunos/salvar", methods=["POST"])
 def registrar_aluno():
-	nome = request.form.get("nome")
-	cpf = request.form.get("cpf")
-	dt_nasc = request.form.get("data")
-	email = request.form.get("e_mail")
-	curso = request.form.get("curso")
+	# Lista contendo todos os erros cometidos pelo usuário
+	erros = []
+	# Exibe uma mensagem de sucesso caso não haja dados dentro da lista de erros
+	sucesso = None
+	# Dicionário contendo todas as informações do aluno
+	aluno = {}
 
-	return render_template("sucesso.html")
+	if request.method == "POST":
+		aluno = {
+		'nome': request.form.get("nome").strip(),
+		'cpf': request.form.get("cpf").strip(),
+		'dt_nasc': request.form.get("data"),
+		'email': request.form.get("e_mail"),
+		'curso': request.form.get("curso")
+		}
+
+	#### Validar NOME ####
+
+	#Divide o texto em partes menores usando, por padrão, os espaços entre as palavras
+	partes_nome = aluno['nome'].split()
+
+	if not aluno['nome']:
+		erros.append("O nome não pode ser composto apenas por espaços.")
+	else:
+		if len(partes_nome) < 2:
+			erros.append("Deve possuir pelo menos NOME e SOBRENOME.")
+		if any(char.isdigit() for char in aluno['nome']):
+			erros.append("O nome não deve conter números.")
+
+	#### Validar CPF ####
+
+	if len(aluno['cpf']) < 11:
+		erros.append("O CPF deve conter 11 dígitos númericos.")
+
+	#### Validar DATA de NASCIMENTO ####
+
+	data_atual = date.today()
+	# Transforma a data em formato de texto (string) em uma data reconhecida pelo Python
+	data_nasc_convertida = datetime.strptime(aluno['dt_nasc'], "%Y-%m-%d").date()
+
+	if data_nasc_convertida > data_atual:
+		erros.append("A data de nascimento não pode representar uma data futura.")
+	else:
+		idade = data_atual.year - data_nasc_convertida.year
+
+		if idade < 14:
+			erros.append("O aluno deve possuir idade mínima de 14 anos.")
+
+	if not erros:
+		sucesso = "Cadastro realizado com sucesso!"
+
+	context = {
+	"sucesso": sucesso,
+	"erros": erros,
+	"aluno": aluno
+	}
+
+	return render_template("sucesso.html", **context)
 
 ##########################################################
 
